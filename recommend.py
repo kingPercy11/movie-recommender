a9d1f6eb4017c08e52f31ed8d075eeff011b0476
Pranjal Chaurasia <pranjalchaurasia11@gmail.com>
2025-04-19 03:57:59 +0530

    model complete

--- recommend.py
+++ recommend.py
@@ -1,7 +1,12 @@
 import numpy as np
 import pandas as pd
 import ast
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import pickle
 
+
+# //dataset
 movies = pd.read_csv('./datasets/tmdb_5000_movies.csv')
 credits = pd.read_csv('./datasets/tmdb_5000_credits.csv')
 
@@ -17,6 +22,8 @@ movies = movies[['movie_id', 'title', 'overview', 'genres', 'keywords', 'cast', 
 movies.dropna(inplace=True) 
 # print(movies.isnull().sum())
 
+
+# preprocessing
 def convert(obj):
     L=[]
     for i in ast.literal_eval(obj):
@@ -59,4 +66,23 @@ movies['overview'] = movies['overview'].apply(lambda x:x.split())
 movies['tags'] = movies['overview'] + movies['genres'] + movies['keywords'] + movies['cast'] + movies['crew']
 new = movies.drop(columns=['overview','genres','keywords','cast','crew'])
 new['tags'] = new['tags'].apply(lambda x: " ".join(x))
-print(new.head())
+# print(new.head())
+
+
+# vectorization and model training
+cv = CountVectorizer(max_features=5000,stop_words='english')
+vector = cv.fit_transform(new['tags']).toarray()
+similarity = cosine_similarity(vector)
+
+def recommend(movie):
+    index = new[new['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])),reverse=True,key = lambda x: x[1])
+    for i in distances[1:6]:
+        print(new.iloc[i[0]].title)
+
+# recommend('The Dark Knight Rises')
+
+
+# save the model
+pickle.dump(new,open('movie_list.pkl','wb'))
+pickle.dump(similarity,open('similarity.pkl','wb'))
